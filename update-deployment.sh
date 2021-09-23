bb98e5d63674e4fe21f64e8245ff00bca955d4ea
etiennep <etienne.pelletier@segment.com>
2021-09-23 10:07:40 -0700

    Cleanup deployment update steps

--- update-deployment.sh
+++ update-deployment.sh
@@ -8,6 +8,6 @@ mkdir -p output/workloads
 
 echo "Updating deployment with image tag ${TAG}"
 
-sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml
+#sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml
 
 sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml > output/workloads/app.yaml
