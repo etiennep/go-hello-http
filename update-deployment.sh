5f5f8353ef156c46df0ccc4b97fc1592be6366b9
etiennep <etienne.pelletier@segment.com>
2021-09-22 18:06:04 -0700

    Only update deployment when not a PR

--- update-deployment.sh
+++ update-deployment.sh
@@ -3,7 +3,7 @@
 TAG=$1
 
 rm -rf output
-mkdir output
+mkdir -p output/workloads
 
-sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml
-#sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml > output/app.yaml
+#sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml
+sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml > output/workloads/app.yaml
