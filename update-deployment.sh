de81001cacb20f7ae1a57c8aadec404e414c9bf5
etiennep <etienne.pelletier@segment.com>
2021-09-23 09:39:17 -0700

    Debugging update script

--- update-deployment.sh
+++ update-deployment.sh
@@ -5,5 +5,8 @@ TAG=$1
 rm -rf output
 mkdir -p output/workloads
 
-#sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml
+echo "Updating deployment with image tag ${TAG}"
+
+sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml
+
 sed "s/go-hello-http:latest/go-hello-http:${TAG}/g" kubernetes/app.yaml > output/workloads/app.yaml
